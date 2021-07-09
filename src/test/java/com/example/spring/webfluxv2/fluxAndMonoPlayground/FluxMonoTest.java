8ac504c5ad3aac7516de6df9e9a37765bcc78714
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-09 22:07:57 +0400

    simple flux test complete

--- src/test/java/com/example/spring/webfluxv2/fluxAndMonoPlayground/FluxMonoTest.java
+++ src/test/java/com/example/spring/webfluxv2/fluxAndMonoPlayground/FluxMonoTest.java
@@ -3,12 +3,21 @@ package com.example.spring.webfluxv2.fluxAndMonoPlayground;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class FluxMonoTest {
 
     @Test
     public void fluxTest() {
-        Flux.just("1", "2", "3").subscribe(System.out::println);
+        Flux.just("1", "2", "3")
+                .concatWith(Flux.just("str"))
+//                .concatWith(Flux.error(new RuntimeException("Test error")))
+                .log()
+                .concatWith(Mono.just(" mono"))
+                .subscribe(
+                        System.out::println,
+                        (e) -> System.err.println(e.getMessage()),
+                        () -> System.out.println("Complete"));
     }
 
 }
