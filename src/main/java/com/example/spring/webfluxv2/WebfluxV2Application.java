e2669abaef9a832c123db2560d6fb1ef4d659214
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-09 19:42:12 +0400

    init

---
+++ src/main/java/com/example/spring/webfluxv2/WebfluxV2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring.webfluxv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/webfluxv2/WebfluxV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.webfluxv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
