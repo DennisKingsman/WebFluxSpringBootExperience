480613405893a2645284f4a61011b536c9c15c27
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-07-10 02:00:35 +0400

    reactive tests

--- src/test/java/com/example/spring/webfluxv2/fluxAndMonoPlayground/FluxMonoTest.java
+++ src/test/java/com/example/spring/webfluxv2/fluxAndMonoPlayground/FluxMonoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class FluxMonoTest {
 
@@ -18,6 +19,41 @@ public class FluxMonoTest {
                         System.out::println,
                         (e) -> System.err.println(e.getMessage()),
                         () -> System.out.println("Complete"));
+    }
+
+    @Test
+    public void fluxElementsWithoutErrorTest() {
+        Flux<String> flux = Flux
+                .just("one", "two", "three")
+                .log();
+        //if change the order then test fails
+        StepVerifier.create(flux)
+                .expectNext("one", "two", "three")
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxElementsWithErrorTest() {
+        Flux<String> flux = Flux
+                .just("one", "two", "three")
+                .concatWith(Flux.error(new RuntimeException()))
+                .log();
+        StepVerifier.create(flux)
+                .expectNext("one")
+                .expectNext("two")
+                .expectNext("three")
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    public void fluxElementsCountTest() {
+        Flux<String> flux = Flux
+                .just("one", "two", "three")
+                .log();
+        StepVerifier.create(flux)
+                .expectNextCount(3)
+                .verifyComplete();
     }
 
 }
